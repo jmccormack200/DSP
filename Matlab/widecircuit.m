76e49fc01dca44d5554df3800c3d49d72b88320d
John McCormack <mccormack.wgsi@gmail.com>
2016-04-21 14:14:08 -0400

    moving through the project

--- Matlab/widecircuit.m
+++ Matlab/widecircuit.m
@@ -7,3 +7,17 @@ fourier = fft(y);
 chanL = fourier(:,1);
 chanR = fourier(:,2);
 
+phaseL = unwrap(angle(chanL) + pi);
+phaseR = unwrap(angle(chanR) + pi);
+
+magL = abs(chanL)*0.8;
+magR = abs(chanR)*0.8;
+
+[rL, cL] = pol2cart(phaseL, magL);
+[rR, cR] = pol2cart(phaseR, magR);
+
+midL = rL + cL * 1j;
+midR = rR + cR * 1j;
+
+
+
